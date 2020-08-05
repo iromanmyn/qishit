d0600826e5865173c7fbd15462ed4bcba825755f
iromanmyn <872490574@qq.com>
2020-08-05 21:19:07 +0800

    first commit

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports={
+    devServer:{
+        open:true,
+        proxy:{
+            '/':{
+                target:'http://localhost:3000',
+                changeOrigin:true,
+            }
+        }
+    },
+    lintOnSave: false,
+}
